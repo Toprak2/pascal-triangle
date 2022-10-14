267ff9e3a9d7ce21ac17208cd12730d39cc8012d
Toprak2 <muhammedalitoprak1@gmail.com>
2022-10-14 22:51:26 +0300

    Proje dosyası ekle.

---
+++ pascal üçgeni/pascal üçgeni.cpp
@@ -0,0 +1,22 @@
+﻿using namespace std;
+#include <iostream>
+int sutuninput;
+int satirinput;
+int main()
+{
+	unsigned long long faktoriyel = 1;
+	unsigned long long faktoriyel2 = 1;
+	cout << "satır numarasini girin:\n";
+	cin >> satirinput;
+	cout << "sutun numarasini girin:\n";
+	cin >> sutuninput;
+	int sutun = sutuninput - 1;
+	int satir = satirinput - 1;
+	for (sutun; sutun > 0; sutun--) {
+		faktoriyel = satir * faktoriyel;
+		satir--;
+		faktoriyel2 = sutun * faktoriyel2;
+	}
+	cout << faktoriyel / faktoriyel2;
+}
+
