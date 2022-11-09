a7e669f4cba115abe2cab23683dd533b8669d5df
Toprak2 <muhammedalitoprak1@gmail.com>
2022-11-09 11:58:14 +0300

    Update pascal üçgeni.cpp

--- pascal üçgeni/pascal üçgeni.cpp
+++ pascal üçgeni/pascal üçgeni.cpp
@@ -5,24 +5,38 @@ int main()
 {
 	int sutun;
 	int satir;
-	
+
 	cout << "enter row number:\n";
 	cin >> satir;
 	cout << "enter column number:\n";
 	cin >> sutun;
 	
-	unsigned long long faktoriyel = 1;
-	unsigned long long faktoriyel2 = 1;
+	int satircons = satir;
+	long long int faktoriyel = 1;
+	long long int faktoriyel2 = 1;
 
+	int k = sutun;
+
+
+	for (k; k >= 0; k--) {
 		for (sutun; sutun > 0; sutun--) {
 			faktoriyel = satir * faktoriyel;
 			satir--;
 			faktoriyel2 = sutun * faktoriyel2;
 			
 		}
-		cout << faktoriyel / faktoriyel2;
+
+		sutun = k-1 ;
+		satir = satircons;
+		cout << faktoriyel / faktoriyel2 << "\t";
+
+		faktoriyel = 1;
+		faktoriyel2 = 1;
+	}
+}
+	
 
 
 	
-}
+
 
